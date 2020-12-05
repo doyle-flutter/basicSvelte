7b3029ecadc56ad2fd895aeb5a9016929d7fab77
doyle <il4479@hanmail.net>
2020-12-05 17:10:53 +0900

    intro

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script>
 	export let name;
+	export let logo;
+	export let github;
+	export let ko;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	<p>{ko}</p>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div><a href="{github}"><img src="{logo}" alt="logo"></a></div>
 </main>
 
 <style>
